95d7e1f12bc3bfc64bb451ddfff3008051fd81f9
Ricardo Pereira <ricardo.agp.914@gmail.com>
2018-05-06 16:15:25 +0100

    Automatic backup at 1525619725.

--- .dotfiles.sh
+++ .dotfiles.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # List of dotfiles to be version controled.
-FILES=( ".vimrc" ".oh-my-zsh/themes/dst.zsh-theme" ".zshrc" ".zsh_plugins.txt" ".gitconfig" ".config/nvim/init.vim" ".dotfiles.sh" )
+FILES=( ".vimrc" ".oh-my-zsh/themes/dst.zsh-theme" "README.md" ".zshrc" ".zsh_plugins.txt" ".gitconfig" ".config/nvim/init.vim" ".dotfiles.sh" )
 GIT_COMMAND="/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
 
 # Wait 5 seconds before attempting to backup dotfiles.
