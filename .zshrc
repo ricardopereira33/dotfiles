ed18b590d9fab3dfbaf1f78a46d5653f5d9af032
Ricardo Pereira <ricardo.agp.914@gmail.com>
2020-11-09 09:12:25 +0000

    Update dotfiles

--- .zshrc
+++ .zshrc
@@ -1,7 +1,9 @@
 DOTFILES_DIR="$HOME/Documents/dotfiles"
+ZSH_DISABLE_COMPFIX=true
 
 for file in $DOTFILES_DIR/zsh/*.zsh; do
   source "$file"
 done
 
 eval "$(starship init zsh)"
+
--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -8,6 +8,7 @@ export PATH="/usr/local/opt/curl/bin:$PATH"
 export PATH="/usr/local/Cellar/qt@5.5/5.5.1_1/bin:$PATH"
 export PATH="/usr/local/Cellar/python/2.7.14_3/bin:$PATH"
 export PATH="/usr/local/go/bin:$PATH"
+export PATH="/usr/local/sbin:$PATH"
 export PATH="$HOME/Library/Python/3.8/bin:$PATH"
 export PATH="$HOME/.rvm/bin:$PATH"
 export PATH="$HOME/.rbenv/bin:$PATH"
@@ -23,7 +24,7 @@ export CPPFLAGS="-I/usr/local/opt/openssl/include -I/usr/local/opt/icu4c/include
 export PKG_CONFIG_PATH="/usr/local/opt/openssl/lib/pkgconfig:/usr/local/opt/icu4c/lib/pkgconfig"
 
 export GPG_TTY=`tty`
-export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_221.jdk/Contents/Home"
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_271.jdk/Contents/Home"
 export GOPATH="$HOME/go"
 export LANG="en_US.UTF-8"
 export ERL_AFLAGS="-kernel shell_history enabled"
